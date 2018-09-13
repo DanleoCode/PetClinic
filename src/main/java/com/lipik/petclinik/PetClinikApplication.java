614ee5fb26c7fa914899c0a6a1db4907db809325
dhananjay tiwari <dhananzait@gmail.com>
2018-09-13 20:45:31 +0530

    first commit

---
+++ src/main/java/com/lipik/petclinik/PetClinikApplication.java
@@ -0,0 +1,12 @@
+package com.lipik.petclinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinikApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lipik/petclinik/PetClinikApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lipik.petclinik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinikApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
