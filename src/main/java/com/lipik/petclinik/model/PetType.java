efeda1b09154934ee3bdd74eb17a71ca95938917
dhananjay tiwari <dhananzait@gmail.com>
2018-09-16 20:11:20 +0530

    adding POJOs.

---
+++ src/main/java/com/lipik/petclinik/model/PetType.java
@@ -0,0 +1,14 @@
+package com.lipik.petclinik.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
