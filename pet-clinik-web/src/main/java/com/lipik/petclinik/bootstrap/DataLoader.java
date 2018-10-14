7421add888f0d8d95524cfe415756481f64c159f
dhananjay tiwari <dhananzait@gmail.com>
2018-10-14 18:41:52 +0530

    added pettype to bootstrap

--- pet-clinik-web/src/main/java/com/lipik/petclinik/bootstrap/DataLoader.java
+++ pet-clinik-web/src/main/java/com/lipik/petclinik/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package com.lipik.petclinik.bootstrap;
 
 
 import com.lipik.petclinik.model.Owner;
+import com.lipik.petclinik.model.PetType;
 import com.lipik.petclinik.model.Vet;
 import com.lipik.petclinik.services.OwnerService;
+import com.lipik.petclinik.services.PetTypeService;
 import com.lipik.petclinik.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Ashok");
         owner1.setLastName("charsi");
